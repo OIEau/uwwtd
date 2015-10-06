ea35f87787010aaaee035db3be71abd1319e781d
Eric Vincent <e.vincent@oieau.fr>
2015-10-06 12:16:49 +0200

    move file uwwtd.js

--- uwwtd website/sites/all/themes/uwwtd/js/uwwtd.js
+++
@@ -1,7 +0,0 @@
-function uwwtd_labelformaterpie(label, series) {
-    return "<div class=\"pie_slice\">" + Math.round(series.percent) + "%</div>";
-}     
-
-function uwwtd_labelformaterpie_legend(label, series) {
-    return "<div class=\"pie_legend\">" + label + " (" + Math.round(series.percent) + "%)</div>";  
-}     
