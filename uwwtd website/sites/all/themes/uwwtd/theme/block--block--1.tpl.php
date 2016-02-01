694a3b8a7dfdc2ad54a2b0db1694056a44b09fd0
Nicolas DHUYGELAERE <nd@webnuxdev.rnde.tm.fr>
2016-02-01 10:28:37 +0100

    front page & stats

--- uwwtd website/sites/all/themes/uwwtd/theme/block--block--1.tpl.php
+++ uwwtd website/sites/all/themes/uwwtd/theme/block--block--1.tpl.php
@@ -1,96 +1,53 @@
+<?php 
+  drupal_add_library('system', 'ui.dialog');
+  drupal_add_library ('system', 'effects.transfer');
+  drupal_add_library ('system', 'effects.puff');
+  drupal_add_library ('system', 'effects.scale');
+?>
+
 <script>
 (function($){
 	$(document).ready(function(){
-		var largeur_fenetre = $(window).width();
-		$('a.btnContent').click(function() {
-			var popID = $(this).attr('rel'); //Trouver la pop-up correspondante
-			var popURL = $(this).attr('href'); //Retrouver la largeur dans le href
-			var imgUrl = "http://" + location.host+ location.pathname;
-			//Récupérer les variables depuis le lien
-			var query= popURL.split('?');
-			var dim= query[1].split('&amp;');
-			var popWidth = dim[0].split('=')[1]; //La première valeur du lien
+    $.fn.bootstrapBtn = $.fn.button.noConflict();
 
-			//Faire apparaitre la pop-up et ajouter le bouton de fermeture
-			$('#' + popID).fadeIn().css({
-				'width': Number(popWidth)
-			})
-			.prepend('<div class="div_btn_close" style="position:fixed;"><a href="#" class="close"><img src='+imgUrl +'sites/all/themes/uwwtd/images/popclose.png class="btn_close" title="Fermer" alt="Fermer" /></a></div>');
+		$( "#popup_content" ).dialog({
+      title: "About the content of this website",
+      autoOpen: false,
+      width: 950,
+  		modal: true,
+  		closeText: "Close",
+      show: {
+        effect: "puff",
+        duration: 250
+      },
+      hide: {
+        effect: "puff",
+        duration: 250
+      }
+    });
+ 
+    $( "a.btnContent" ).click(function() {
+      $( "#popup_content" ).dialog( "open" );
+    });
 
-			if(largeur_fenetre < 1350){
-				$('.popup_content_block .div_btn_close').css('right', '9%');
-				$('.popup_content_block .div_btn_close').css('top', '24%');
-			}else{
-				$('.popup_content_block').css('margin-left', '-10%');
-				$('.popup_content_block .div_btn_close').css('right', '8%');
-				$('.popup_content_block .div_btn_close').css('top', '27%');
-			}
-			
-			//Récupération du margin, qui permettra de centrer la fenêtre - on ajuste de 80px en conformité avec le CSS
-			var popMargTop = ($('#' + popID).height() + 80) / 2;
-			var popMargLeft = ($('#' + popID).width() + 80) / 2;
-
-			//Effet fade-in du fond opaque
-			$('body').append('<div id="fade"></div>'); //Ajout du fond opaque noir
-			//Apparition du fond - .css({'filter' : 'alpha(opacity=80)'}) pour corriger les bogues de IE
-			 $('#fade').css({'filter' : 'alpha(opacity=80)'}).fadeIn();
-
-			return false;
-		});
-		//Fermeture de la pop-up et du fond
-		$('a.close, #fade').live('click', function() { //Au clic sur le bouton ou sur le calque...
-			$('#fade , .popup_content_block').fadeOut(function() {
-				$('#fade, a.close').remove();  //...ils disparaissent ensemble
-			});
-			return false;
-		});
-		
-		//Btn project
-		$('a.btnProject').click(function() {
-			
-				
-			var popID = $(this).attr('rel'); //Trouver la pop-up correspondante
-			var popURL = $(this).attr('href'); //Retrouver la largeur dans le href
-			var imgUrl = "http://" + location.host+ location.pathname;
-			//Récupérer les variables depuis le lien
-			var query= popURL.split('?');
-			var dim= query[1].split('&amp;');
-			var popWidth = dim[0].split('=')[1]; //La première valeur du lien
-
-			//Faire apparaitre la pop-up et ajouter le bouton de fermeture
-			$('#' + popID).fadeIn().css({
-				'width': Number(popWidth)
-			})
-			.prepend('<div class="div_btn_close" style="position:fixed;"><a href="#" class="closeProject"><img src='+imgUrl +'sites/all/themes/uwwtd/images/popclose.png class="btn_close" title="Fermer" alt="Fermer" /></a></div>');
-
-			if(largeur_fenetre < 1350){
-				$('.popup_project_block').css('margin-left', '5%');
-				$('.popup_project_block .div_btn_close').css('right', '16%');
-				$('.popup_project_block .div_btn_close').css('top', '2%');
-			}else{
-				$('.popup_project_block').css('margin-left', '-5%');
-				$('.popup_project_block .div_btn_close').css('right', '14%');
-				$('.popup_project_block .div_btn_close').css('top', '6%');
-			}
-			//Récupération du margin, qui permettra de centrer la fenêtre - on ajuste de 80px en conformité avec le CSS
-			var popMargTop = ($('#' + popID).height() + 80) / 2;
-			var popMargLeft = ($('#' + popID).width() + 80) / 2;
-
-			//Effet fade-in du fond opaque
-			$('body').append('<div id="fade"></div>'); //Ajout du fond opaque noir
-			//Apparition du fond - .css({'filter' : 'alpha(opacity=80)'}) pour corriger les bogues de IE
-			 $('#fade').css({'filter' : 'alpha(opacity=80)'}).fadeIn();
-
-			return false;
-		});
-		//Fermeture de la pop-up et du fond
-		$('a.closeProject, #fade').live('click', function() { //Au clic sur le bouton ou sur le calque...
-			$('#fade , .popup_project_block').fadeOut(function() {
-				$('#fade, a.closeProject').remove();  //...ils disparaissent ensemble
-			});
-			return false;
-		});
-		
+		$( "#popup_project" ).dialog({
+      title: "About the content of this website",
+      autoOpen: false,
+      width: 950,
+  		modal: true,
+      show: {
+        effect: "puff",
+        duration: 250
+      },
+      hide: {
+        effect: "puff",
+        duration: 250
+      }
+    });
+ 
+    $( "a.btnProject" ).click(function() {
+      $( "#popup_project" ).dialog( "open" );
+    });	
 	});
 })(jQuery);
 </script>
@@ -114,8 +71,8 @@
 	?>
 	<div class ="txtHomePage"><?php print $nat_stat_str; ?></div>
 	<div class="btnHomePage">
-		<a href="#?w=600" class="btnContent" rel="popup_content">About the content of this website</a>
-		<a href="#?w=800" class="btnProject" rel="popup_project">About the SIIF project at the origin of this IT development</a>
+		<a class="btnContent" rel="popup_content">About the content of this website</a>
+		<a class="btnProject" rel="popup_project">About the SIIF project at the origin of this IT development</a>
 	</div>
 	
 	<div id="popup_content" class="popup_content_block">
@@ -132,6 +89,9 @@
 	</div>
 	<div id="popup_project" class="popup_project_block">
 		<div>
+			<span class="ui-helper-hidden-accessible"><input type="text"/></span>
+
+
 			<p>The Structured Implementation and Information Framework (SIIF) concept is a ongoing European Commission (Environment Directorate General) 
 			pilot project essentially focused on the organisation and management of data in order to enhance the generation of information for policy makers, 
 			interested parties and the public at all levels on how legislation is practically implemented. 
@@ -144,9 +104,7 @@
 			(2) the process for data providers (who does what and how); (3) the dissemination for data users (how to display) 
 			All these components have a national and a European dimension. </p>
 			
-			<figure tabindex="1" contenteditable="true">
-				<img id="imgProjectA" src="sites/all/themes/uwwtd/images/pageaccueil.png" contenteditable="false"/>
-			</figure>
+			<p id="imgProjectA"><img src="<?php print base_path(); ?>sites/all/themes/uwwtd/images/pageaccueil.png" contenteditable="false"/></p>
 
 			<p>This IT development is related to the process and the dissemination of the existing content of the European urban waste water data model.</p>
 			<p>The SIIF project opted for the implementation of an open source dissemination IT toolbox. 
