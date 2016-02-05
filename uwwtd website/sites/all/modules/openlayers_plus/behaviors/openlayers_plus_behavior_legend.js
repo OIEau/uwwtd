9ab17cef1d3d1221c02f02f631884af307ddb49f
Nicolas DHUYGELAERE <nd@webnuxdev.rnde.tm.fr>
2016-02-05 15:48:32 +0100

    fix multi style for multi layers scale points

--- uwwtd website/sites/all/modules/openlayers_plus/behaviors/openlayers_plus_behavior_legend.js
+++ uwwtd website/sites/all/modules/openlayers_plus/behaviors/openlayers_plus_behavior_legend.js
@@ -37,7 +37,6 @@ Drupal.OpenLayersPlusLegend.setLegend = function(layer) {
   var legend = data.map.behaviors.openlayers_plus_behavior_legend[name];
   var legends = $('div.openlayers-legends', map);
   if (layer.visibility && $('#openlayers-legend-' + name, legends).size() === 0) {
-      console.log(legend);
     legends.append(legend);
   }
   else if (!layer.visibility) {
