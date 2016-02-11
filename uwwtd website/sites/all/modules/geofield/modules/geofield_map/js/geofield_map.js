cd1272d47339e1797d579c9b937ab9ef064e5d15
vgo <vgo@emakina.fr>
2016-02-11 10:15:57 +0100

    #158

--- uwwtd website/sites/all/modules/geofield/modules/geofield_map/js/geofield_map.js
+++ uwwtd website/sites/all/modules/geofield/modules/geofield_map/js/geofield_map.js
@@ -125,7 +125,15 @@
             var bounds = feature.get('bounds');
             google.maps.event.addListener(feature, 'click', function() {
               infowindow.setPosition(bounds.getCenter());
-              infowindow.setContent(properties.description);
+              
+              // Réutilisation du zoom définit pour cette carte :
+              var content = properties.description;
+              if (typeof map.zoom !== 'undefined') { 
+            	  content = content.replace('zoomValueToBeReplaced', map.zoom + 2);
+              } else {
+            	  content = content.replace('zoomValueToBeReplaced', '16');
+              }
+              infowindow.setContent(content);
               infowindow.open(map);
             });
           }
