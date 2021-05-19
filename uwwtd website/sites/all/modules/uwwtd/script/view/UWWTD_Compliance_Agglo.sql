a14a233a21fd586cf757f6639381de13d6c0106a
Root webnuxdev <root@webnuxdev.rnde.tm.fr>
2021-05-19 18:48:20 +0200

    add fields in wfs views

--- uwwtd website/sites/all/modules/uwwtd/script/view/UWWTD_Compliance_Agglo.sql
+++ uwwtd website/sites/all/modules/uwwtd/script/view/UWWTD_Compliance_Agglo.sql
@@ -3,8 +3,10 @@
     repcode.field_repcode_value AS "repCode",
     comp.field_aggcompliance_value AS caggtreated,
     art3.field_aggart3compliance_value AS cagg3,
+    art3add.field_aggart3addcompliance_value AS cagg3,
     art4.field_aggart4compliance_value AS cagg4,
     art5.field_aggart5compliance_value AS cagg5,
+    art6.field_aggart6compliance_value AS cagg6,
     annee.field_anneedata_value AS "repReportedPerdiod"
    FROM drupal_node n
      LEFT JOIN drupal_field_data_field_siteid siteid ON n.nid = siteid.entity_id
@@ -13,6 +15,9 @@
      LEFT JOIN drupal_field_data_field_inspireidlocalid localid ON n.nid = localid.entity_id
      LEFT JOIN drupal_field_data_field_aggcompliance comp ON n.nid = comp.entity_id
      LEFT JOIN drupal_field_data_field_aggart3compliance art3 ON n.nid = art3.entity_id
+     LEFT JOIN drupal_field_data_field_aggart3addcompliance art3add ON n.nid = art3add.entity_id
      LEFT JOIN drupal_field_data_field_aggart4compliance art4 ON n.nid = art4.entity_id
      LEFT JOIN drupal_field_data_field_aggart5compliance art5 ON n.nid = art5.entity_id
+     LEFT JOIN drupal_field_data_field_aggart6compliance art6 ON n.nid = art6.entity_id
+
   WHERE n.type::text = 'agglomeration'::text AND n.status = 1;
--- uwwtd website/sites/all/modules/uwwtd/script/view/UWWTD_Compliance_UWWTP.sql
+++ uwwtd website/sites/all/modules/uwwtd/script/view/UWWTD_Compliance_UWWTP.sql
@@ -2,6 +2,24 @@
     localid.field_inspireidlocalid_value AS "uwwCode",
     repcode.field_repcode_value AS "repCode",
     comp.field_uwwcompliance_value AS cuwwtreated,
+        CASE
+            WHEN ttype_r.field_uwwtreatmentrequired_value::text = 'P'::text THEN 'Primary'::text
+            WHEN ttype_r.field_uwwtreatmentrequired_value::text = 'S'::text THEN 'Secondary'::text
+            WHEN ttype_r.field_uwwtreatmentrequired_value::text = 'MS'::text THEN 'More Stringent: Other'::text
+            WHEN ttype_r.field_uwwtreatmentrequired_value::text = 'O'::text THEN 'More Stringent'::text
+            WHEN ttype_r.field_uwwtreatmentrequired_value::text = 'MP'::text THEN 'More Stringent: Phosphorus'::text
+            WHEN ttype_r.field_uwwtreatmentrequired_value::text = 'PO'::text THEN 'More Stringent: Phosphorus and Other'::text
+            WHEN ttype_r.field_uwwtreatmentrequired_value::text = 'N'::text THEN 'More Stringent: Nitrogen'::text
+            WHEN ttype_r.field_uwwtreatmentrequired_value::text = 'NO'::text THEN 'More Stringent: Nitrogen and Other'::text
+            WHEN ttype_r.field_uwwtreatmentrequired_value::text = 'NP'::text THEN 'More Stringent: Nitrogen and Phosphorus'::text
+            WHEN ttype_r.field_uwwtreatmentrequired_value::text = 'NPO'::text THEN 'More Stringent: Nitrogen, Phosphorus and Other'::text
+            WHEN ttype_r.field_uwwtreatmentrequired_value::text = 'NR'::text THEN 'Not relevant'::text
+            WHEN ttype_r.field_uwwtreatmentrequired_value::text = 'Appropriate'::text THEN 'Appropriate'::text
+            ELSE NULL::text
+        END AS "uwwTreatmentTypeRequired",
+    treat.field_uwwtreatment_met_value as "uwwTreatmentMet",
+    perf.field_uwwperformance_met_value as "uwwPerformanceMet",
+    
     annee.field_anneedata_value AS "repReportedPerdiod"
    FROM drupal_node n
      LEFT JOIN drupal_field_data_field_siteid siteid ON n.nid = siteid.entity_id
@@ -9,4 +27,7 @@
      LEFT JOIN drupal_field_data_field_repcode repcode ON n.nid = repcode.entity_id
      LEFT JOIN drupal_field_data_field_inspireidlocalid localid ON n.nid = localid.entity_id
      LEFT JOIN drupal_field_data_field_uwwcompliance comp ON n.nid = comp.entity_id
+     LEFT JOIN drupal_field_data_field_uwwtreatmentrequired ttype_r ON n.nid = ttype_r.entity_id
+     LEFT JOIN drupal_field_data_field_uwwperformance_met  perf ON n.nid = perf.entity_id
+     LEFT JOIN drupal_field_data_field_uwwtreatment_met treat ON n.nid = treat.entity_id
   WHERE n.type::text = 'uwwtp'::text AND n.status = 1;
